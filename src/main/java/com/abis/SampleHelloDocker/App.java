a8496ddc54e0fb2baa38ed5bdba44fe3ea1f55e2
Abismita <abismita333@gmail.com>
2022-09-27 11:09:09 +0530

    first commit

---
+++ src/main/java/com/abis/SampleHelloDocker/App.java
@@ -0,0 +1,13 @@
+package com.abis.SampleHelloDocker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, Welcome to Docker" );
+    }
+}
